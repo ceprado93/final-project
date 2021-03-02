d64602f046c8cac6c97bfe413e318af8ff78c0ef
eva virseda <evavi@MacBook-Air.home>
2021-03-02 14:05:46 +0100

    deploy heroku

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const MONGODB_URI = process.env.MONGODB_URI || `mongodb://localhost/final-project`
+const MONGODB_URI = process.env.DB_REMOTE || `mongodb://localhost/final-project`
 
 mongoose
     .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
