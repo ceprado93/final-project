7b6980ee03a5a782b9d6f4cd9647b9c50959ab20
Carlos Prado <ceprado93@gmail.com>
2021-03-04 15:03:16 +0000

    first markers added

--- client/src/components/pages/Map/Map.js
+++ client/src/components/pages/Map/Map.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import waveService from './../../../service/wave.service'
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
@@ -11,8 +12,11 @@ class Map extends Component {
         lat: undefined,
         lng: undefined
       },
+      waves: [],
+      ready: false,
       zoom: undefined
     }
+    this.waveService = new waveService()
   }
   componentDidMount() {
     this.setValues()
@@ -25,23 +29,38 @@ class Map extends Component {
   //     zoom: 1
   //   };
   setValues() {
-    this.setState({ center: { lat: this.props.lat, lng: this.props.lng }, zoom: this.props.zoom })
+    this.setState({
+      center: { lat: this.props.lat, lng: this.props.lng },
+      zoom: this.props.zoom
+    }, () => {
+      this.waveService
+        .getWaves()
+        .then(response => {
+          this.setState({ waves: response.data, ready: true })
+          console.log(response.data[2].location.coordinates)
+        })
+        .catch(err => console.log(err))
+    })
+
   }
+
   render() {
     return (
       // Important! Always set the container height explicitly
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: '' }}
+          bootstrapURLKeys={{ key: 'AIzaSyDWox-Ew5Z4Wm2OMqZSFRhM-IIwzPtxRgU' }}
           defaultCenter={this.state?.center}
           defaultZoom={this.state?.zoom}
         >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text="My Marker"
-          />
+          {this.state.ready && this.state.waves.map(elm => <AnyReactComponent
+            key={elm._id}
+            lat={elm.location.coordinates[0]}
+            lng={elm.location.coordinates[1]}
+            text={elm.title}
+          />)}
         </GoogleMapReact>
+
       </div>
     );
   }
