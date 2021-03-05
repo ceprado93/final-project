9e2303a7cd51b1157e8695db6fa9c8cb76b5d060
Carlos Prado <ceprado93@gmail.com>
2021-03-05 09:43:46 +0000

    display changes

--- client/src/components/pages/Regions/Continents.js
+++ client/src/components/pages/Regions/Continents.js
@@ -1,39 +1,74 @@
-import { Container, Row, Col } from 'react-bootstrap'
+import { Component } from 'react'
+import { Container, Row, Col, Button, ButtonGroup } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import Map from './../Map/Map'
-const Continents = () => {
+import './Continents.css'
 
-    return (
-        <>
-            <section className="first-sect" style={{ marginTop: 100 }}>
-                <h1>Continents</h1>
-                <p>the continents and their waves</p>
-            </section>
-            <section style={{ marginTop: 100 }}>
-                <Container>
-                    <Map lat={26.907911} lng={-32.725575} zoom={1}>
-                    </Map>
-                    <Row>
-                        <Col>
-                            <ul>
-                                <li><Link to={{ pathname: '/Europe', state: { lat: 52.310768, lng: 8.679055, zoom: 4 } }}>Europe</Link></li>
-                                <li><Link to={{ pathname: '/Africa', state: { lat: 1.667755, lng: 21.262689, zoom: 3 } }} >Africa</Link></li>
-                                <li> <Link to={{ pathname: '/Asia', state: { lat: 22.908563, lng: 105.228742, zoom: 3 } }} >Asia</Link></li>
-                            </ul>
-                        </Col>
-                        <Col>
-                            <ul>
-                                <li><Link to={{ pathname: '/AustraliaNZ', state: { lat: -30.697441, lng: 142.698648, zoom: 4 } }}>Australia and NZ</Link></li>
-                                <li> <Link to={{ pathname: '/Pacific', state: { lat: 12.094258, lng: - 165.853220, zoom: 3 } }} >Pacific</Link></li>
-                                <li> <Link to={{ pathname: '/America', state: { lat: 9.795061, lng: - 83.979147, zoom: 2 } }} >America</Link></li>
-                            </ul>
-                        </Col>
-                    </Row>
-                </Container>
-            </section>
-        </>
+class Continents extends Component {
 
-    )
+    constructor() {
+        super()
+        this.state = {
+            mapClass: 'notSeeMap',
+            listClass: 'seeList'
+        }
+    }
+
+    showMap() {
+        this.setState({
+            mapClass: 'seeMap',
+            listClass: 'notSeeList'
+        })
+    }
+
+    showList() {
+        this.setState({
+            mapClass: 'notSeeMap',
+            listClass: 'seeList'
+        })
+    }
+
+    render() {
+        return (
+            <>
+                <section className="first-sect" >
+                    <h1 >Continents</h1>
+                    <p>the continents and their waves</p>
+                </section>
+                <section style={{ marginTop: 100 }}>
+
+                    <Container>
+                        <ButtonGroup size="mb" style={{ marginBottom: 20 }}>
+                            <Button variant="dark" onClick={() => this.showMap()}> Map</Button>
+                            <Button variant="outline-dark" onClick={() => this.showList()}>List</Button>
+                        </ButtonGroup>
+                        <div className={this.state.mapClass} >
+                            <Map lat={26.907911} lng={-32.725575} zoom={1} />
+                        </div>
+                        <div className={this.state.listClass}>
+                            <Row >
+                                <Col>
+                                    <ul>
+                                        <li><Link to={{ pathname: '/Europe', state: { lat: 52.310768, lng: 8.679055, zoom: 4 } }}>Europe</Link></li>
+                                        <li><Link to={{ pathname: '/Africa', state: { lat: 1.667755, lng: 21.262689, zoom: 3 } }} >Africa</Link></li>
+                                        <li> <Link to={{ pathname: '/Asia', state: { lat: 22.908563, lng: 105.228742, zoom: 3 } }} >Asia</Link></li>
+                                    </ul>
+                                </Col>
+                                <Col>
+                                    <ul>
+                                        <li><Link to={{ pathname: '/AustraliaNZ', state: { lat: -30.697441, lng: 142.698648, zoom: 4 } }}>Australia and NZ</Link></li>
+                                        <li> <Link to={{ pathname: '/Pacific', state: { lat: 12.094258, lng: - 165.853220, zoom: 3 } }} >Pacific</Link></li>
+                                        <li> <Link to={{ pathname: '/America', state: { lat: 9.795061, lng: - 83.979147, zoom: 2 } }} >America</Link></li>
+                                    </ul>
+                                </Col>
+                            </Row>
+                        </div>
+                    </Container>
+                </section>
+            </>
+
+        )
+    }
 }
 
 export default Continents
--- client/src/components/pages/Regions/Regions.js
+++ client/src/components/pages/Regions/Regions.js
@@ -1,10 +1,12 @@
 import { Component } from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, ButtonGroup, Button } from 'react-bootstrap'
 
 import { Link } from 'react-router-dom'
 import WaveService from '../../../service/wave.service'
 import Spinner from '../../shared/Spinner/Spinner'
 import Map from './../Map/Map'
+import './Continents.css'
+
 
 class Regions extends Component {
 
@@ -13,6 +15,8 @@ class Regions extends Component {
         this.state = {
             region: [],
             numberOfWaves: 0,
+            mapClass: 'notSeeMap',
+            listClass: 'seeList'
 
         }
         this.waveService = new WaveService()
@@ -43,20 +47,40 @@ class Regions extends Component {
             .catch(err => console.log(err))
     }
 
+    showMap() {
+        this.setState({
+            mapClass: 'seeMap',
+            listClass: 'notSeeList'
+        })
+    }
+
+    showList() {
+        this.setState({
+            mapClass: 'notSeeMap',
+            listClass: 'seeList'
+        })
+    }
+
     render() {
         return (
             <>
                 <Container as="section">
-                    <Map lat={this.props.history.location.state.lat} lng={this.props.history.location.state.lng} zoom={this.props.history.location.state.zoom}></Map>
+                    <ButtonGroup size="mb" style={{ marginBottom: 20 }}>
+                        <Button variant="dark" onClick={() => this.showMap()}> Map</Button>
+                        <Button variant="outline-dark" onClick={() => this.showList()}>List</Button>
+                    </ButtonGroup>
+                    <div className={this.state.mapClass} >
+                        <Map lat={this.props.history.location.state.lat} lng={this.props.history.location.state.lng} zoom={this.props.history.location.state.zoom} />
+                    </div>
                     {this.state.region
 
                         ?
-                        <Row>
+                        <Row className={this.state.listClass}>
+                            <hr />
                             <Col md={{ span: 6 }}>
                                 <ul>
                                     {this.state.region.map((elm, idx) => <li key={idx}> <Link to={`/waves/${elm}`} {...this.props} >{elm}</Link></li>)}
                                 </ul>
-                                <hr />
                             </Col>
                         </Row>
                         :
